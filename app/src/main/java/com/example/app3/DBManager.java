fc3371354554fea59a46b85e952e7f67aba89304
Ding Xinghui <hitapple271@gmail.com>
2020-08-11 13:04:29 +0800

    增加了本地数据库的相关代码，包括DBManager,HospitalWeb,添加了litepal依赖

---
+++ app/src/main/java/com/example/app3/DBManager.java
@@ -0,0 +1,64 @@
+package com.example.app3;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class DBManager {
+
+    private static final String DB_NAME = "HospitalWebsite.db"; //保存的数据库文件名
+    private static final String PACKAGE_NAME = "com.example.app3";//包名
+    private static final String DB_PATH = "/data"
+            + Environment.getDataDirectory().getAbsolutePath() + "/"
+            + PACKAGE_NAME + "/databases";  //存放数据库的位置
+    private SQLiteDatabase database;
+    private Context context;
+
+    public DBManager(Context context) {
+        this.context = context;
+    }
+
+    public void openDatabase() {
+        File dFile = new File(DB_PATH);//判断路径是否存在，不存在则创建路径
+        if (!dFile.exists()) dFile.mkdir();
+//        this.database = this.openDatabase(DB_PATH + "/" + DB_NAME);
+        this.openDatabase(DB_PATH + "/" + DB_NAME);
+    }
+
+    private SQLiteDatabase openDatabase(String dbfile) {
+        try {
+            if (!(new File(dbfile).exists())) {
+                InputStream is = this.context.getResources().getAssets().open("HospitalWebsite.db"); //欲导入的数据库
+                FileOutputStream fos = new FileOutputStream(dbfile);
+                int BUFFER_SIZE = 1028 * 10;
+                byte[] buffer = new byte[BUFFER_SIZE];
+                int count = 0;
+                while ((count = is.read(buffer)) > 0) {
+                    fos.write(buffer, 0, count);
+                }
+                fos.close();
+                is.close();
+            }
+            return SQLiteDatabase.openOrCreateDatabase(dbfile,
+                    null);
+        } catch (FileNotFoundException e) {
+            Log.e("Database", "File not found");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e("Database", "IO exception");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void closeDatabase() {
+        this.database.close();
+    }
+}
---
+++ app/src/main/java/com/example/app3/HospitalWeb.java
@@ -0,0 +1,34 @@
+package com.example.app3;
+
+import org.litepal.crud.LitePalSupport;
+
+public class HospitalWeb extends LitePalSupport {
+
+    private int id;
+    private String name;
+    private String website;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+}
